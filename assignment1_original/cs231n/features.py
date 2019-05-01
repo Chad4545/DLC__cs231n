af7e41ef54e457b3340aa2d53747eb4e9c559c9f
Chad4545 <genom1324@gmail.com>
2019-05-01 11:47:11 +0900

    complete features

--- assignment1_original/cs231n/features.py
+++ assignment1_original/cs231n/features.py
@@ -117,8 +117,8 @@ def hog_feature(im):
                         temp_ori, 0)
     # select magnitudes for those orientations
     cond2 = temp_ori > 0
-    temp_mag = np.where(cond2, grad_mag, 0)
-    orientation_histogram[:,:,i] = uniform_filter(temp_mag, size=(cx, cy))[cx/2::cx, cy/2::cy].T
+    temp_mag = np.where(cond2, grad_mag, 0)                              #numpy version issue
+    orientation_histogram[:,:,i] = uniform_filter(temp_mag, size=(cx, cy))[cx//2::cx, cy//2::cy].T
   
   return orientation_histogram.ravel()
 
