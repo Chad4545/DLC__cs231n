934a45483ff203e1dc8764e22d0cf1f2a4f43f61
Chad4545 <genom1324@gmail.com>
2019-05-03 21:01:22 +0900

    complete FullyConnectedNets

--- assignment2_original/cs231n/optim.py
+++ assignment2_original/cs231n/optim.py
@@ -54,18 +54,30 @@ def sgd_momentum(w, dw, config=None):
       Setting momentum = 0 reduces to sgd.
     - velocity: A numpy array of the same shape as w and dw used to store a
       moving average of the gradients.
+      
+    - how to get velocity : we maintain a velocity over time and we add our gradient estimates to the velocity
+                            and then we step in the direction of velocity rather than stepping in the direction
+                            of the gradient
     """
     if config is None: config = {}
+    # config = {'learning_rate':, 'velocity':}
+    # setdefault => dict에 key없으면 , 우리가 준 key, value로 추가     
     config.setdefault('learning_rate', 1e-2)
     config.setdefault('momentum', 0.9)
+    
+    lr = config.get('learning_rate')
+    rho = config.get('momentum')
+    # key 없으면 ,np.zeros_like(w) 로 value 줌
     v = config.get('velocity', np.zeros_like(w))
+    
 
     next_w = None
     ###########################################################################
     # TODO: Implement the momentum update formula. Store the updated value in #
     # the next_w variable. You should also use and update the velocity v.     #
     ###########################################################################
-    pass
+    v = rho*v - lr*dw # velocity + gradient = actual step
+    next_w = w + v
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -99,7 +111,15 @@ def rmsprop(x, dx, config=None):
     # in the next_x variable. Don't forget to update cache value stored in    #
     # config['cache'].                                                        #
     ###########################################################################
-    pass
+    lr = config.get('learning_rate')
+    decay = config.get('decay_rate')
+    eps = config.get('epsilon')
+    grad_squared = config.get('cache')
+    
+    grad_squared = decay*grad_squared + (1-decay)*(dx**2)
+    next_x = x - (lr*dx)/(np.sqrt(grad_squared+eps))
+    config['cache'] = grad_squared
+    
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -136,7 +156,24 @@ def adam(x, dx, config=None):
     # the next_x variable. Don't forget to update the m, v, and t variables   #
     # stored in config.                                                       #
     ###########################################################################
-    pass
+    lr = config.get('learning_rate')
+    beta_1 = config.get('beta1')
+    beta_2 = config.get('beta2')
+    eps = config.get('epsilon')
+    first_momentum = config.get('m')
+    second_momentum = config.get('v')
+    time = config.get('t')
+    
+    time += 1
+    
+    first_momentum = beta_1 * first_momentum + (1-beta_1)*dx
+    second_momentum = beta_2 * second_momentum + (1-beta_2)*dx*dx
+    first_unbias = first_momentum / (1-beta_1 ** time)
+    second_unbias = second_momentum /(1-beta_2 ** time)
+    next_x = x - lr*first_unbias/(np.sqrt(second_unbias + eps))
+    config['m'] = first_momentum
+    config['v'] = second_momentum
+    config['t'] = time
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
